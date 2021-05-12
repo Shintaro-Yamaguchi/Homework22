b0000f2f5864639660d83751a83db36ffef38341
Shintaro Yamaguchi <Shintaro Yamaguchi@LAPTOP-FH8LBKGF>
2021-05-13 05:55:47 +0900

    5/13(木)更新。

--- src/Control.java
+++ src/Control.java
@@ -1,81 +1,25 @@
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
 public class Control {
 	int x = 0;
 	int y = 0;
-	Scanner scan = new Scanner(System.in);
-	int num;
 	
 	public void north() {
-		if (this.num == 1) {
-			this.y += 1;
-			location();
-		}
-		
+		this.y += 1;
 	}
 	
 	public void east() {
-		if (this.num == 2) {
-			this.x += 1;
-			location();
-		}
-		
+		this.x += 1;
 	}
 	
 	public void south() {
-		if (this.num == 3) {
-			this.y -= 1;
-			location();
-		}
-		
+		this.y -= 1;
 	}
 	
 	public void west() {
-		if (this.num == 4) {
-			this.x -= 1;
-			location();
-		}
-		
+		this.x -= 1;
 	}
 	
 	public void location() {
-		try {
-			this.num = scan.nextInt();
-		
-			if(num == 9) {
-				System.out.println("プログラムを終了します。");
-			}
-			
-			while(true) {
-
-				if(num == 1) {
-					north();
-				} else if(num == 2) {
-					east();
-				} else if(num == 3) {
-					south();
-				} else if(num == 4) {
-					west();
-				} else if(num == 0) {
-					System.out.println("（" + this.x + "、" + this.y + "）");
-					location();
-				} else if(num == 9) {
-					break;
-				} else {
-					System.out.println("対応していない数値です。");
-					location();
-				
-				}
-				
-			}
-		
-		} catch(InputMismatchException e) {
-			System.out.println("数値を入力してください。");
-//			location();
-			
-		}
-			
+		System.out.println("（" + this.x + "、" + this.y + "）");
 	}
-
+	
 }
--- src/Main22.java
+++ src/Main22.java
@@ -1,7 +1,8 @@
-public class Main22 {
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+public class Main22 {
 	public static void main(String[] args) {
-		
 		System.out.println("ロボット歩行プログラムへようこそ。");
 		System.out.println("以下の中からロボットに指示を出してください。");
 		System.out.println("0：現在位置を表示。");
@@ -11,9 +12,43 @@ public class Main22 {
 		System.out.println("4：西へ進む。");
 		System.out.println("9：プログラムを終了。");
 		
+		int num = -1;
 		Control c = new Control();
-		c.location();
-
+		
+		while(true) {
+			try {
+				Scanner scan = new Scanner(System.in);
+				num = scan.nextInt();
+			} catch(InputMismatchException e) {
+				num = -1;
+			}
+			
+			if(num == 1) {
+				c.north();
+			} else if(num == 2) {
+				c.east();
+			} else if(num == 3) {
+				c.south();
+			} else if(num == 4) {
+				c.west();
+			} else if(num == 0) {
+				c.location();
+			} else if(num == 9) {
+				System.out.println("プログラムを終了します。");
+				break;
+			} else {
+				System.out.println("");
+				System.out.println("対応していません。以下の数値を入れてください。");
+				System.out.println("0：現在位置を表示。");
+				System.out.println("1：北へ進む。");
+				System.out.println("2：東へ進む。");
+				System.out.println("3：南へ進む。");
+				System.out.println("4：西へ進む。");
+				System.out.println("9：プログラムを終了。");
+				System.out.println("");
+			}
+		}
+			
 	}
 
 }
