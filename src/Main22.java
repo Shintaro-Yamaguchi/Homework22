15aac6690d45ee6c29c03b01f5755676c4feedeb
Shintaro Yamaguchi <Shintaro Yamaguchi@LAPTOP-FH8LBKGF>
2021-05-13 06:21:26 +0900

    5/13(木)更新。

--- src/Main22.java
+++ src/Main22.java
@@ -12,31 +12,39 @@ public class Main22 {
 		System.out.println("4：西へ進む。");
 		System.out.println("9：プログラムを終了。");
 		
-		int num = -1;
+		int num;
 		Control c = new Control();
+		Scanner scan = new Scanner(System.in);
 		
 		while(true) {
 			try {
-				Scanner scan = new Scanner(System.in);
 				num = scan.nextInt();
-			} catch(InputMismatchException e) {
-				num = -1;
-			}
 			
-			if(num == 1) {
-				c.north();
-			} else if(num == 2) {
-				c.east();
-			} else if(num == 3) {
-				c.south();
-			} else if(num == 4) {
-				c.west();
-			} else if(num == 0) {
-				c.location();
-			} else if(num == 9) {
-				System.out.println("プログラムを終了します。");
-				break;
-			} else {
+				if(num == 1) {
+					c.north();
+				} else if(num == 2) {
+					c.east();
+				} else if(num == 3) {
+					c.south();
+				} else if(num == 4) {
+					c.west();
+				} else if(num == 0) {
+					c.location();
+				} else if(num == 9) {
+					System.out.println("プログラムを終了します。");
+					break;
+				} else {
+					System.out.println("");
+					System.out.println("対応していません。以下の数値を入れてください。");
+					System.out.println("0：現在位置を表示。");
+					System.out.println("1：北へ進む。");
+					System.out.println("2：東へ進む。");
+					System.out.println("3：南へ進む。");
+					System.out.println("4：西へ進む。");
+					System.out.println("9：プログラムを終了。");
+					System.out.println("");
+				}
+			} catch(InputMismatchException e) {
 				System.out.println("");
 				System.out.println("対応していません。以下の数値を入れてください。");
 				System.out.println("0：現在位置を表示。");
@@ -46,7 +54,9 @@ public class Main22 {
 				System.out.println("4：西へ進む。");
 				System.out.println("9：プログラムを終了。");
 				System.out.println("");
+				scan.next();
 			}
+			
 		}
 			
 	}
